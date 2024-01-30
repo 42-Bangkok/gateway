6543846c6b95f251a7d3dc3040b84e80de931bc4
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-01-30 12:36:57 +0000

    Update Python and Django versions

--- playground/base.py
+++ playground/base.py
@@ -3,8 +3,8 @@
 For testing user creation
 """
 import os
-import django
 
+import django
 from appcore.services.utils import slugify
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
