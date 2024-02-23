8efc887cf16f3d104f1eb6819cc2ee86cb739ddb
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-02-24 00:09:42 +0800

    Refactor start.sh script

--- app/start.sh
+++ app/start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [ "$DEPLOYENV" = "dev" ]; then
-    python manage.py migrate && hypercorn app.asgi:application -b 0.0.0.0:8000
-else
-    hypercorn app.asgi:application -b 0.0.0.0:8000
-fi
+python manage.py migrate && hypercorn app.asgi:application -b 0.0.0.0:8000
