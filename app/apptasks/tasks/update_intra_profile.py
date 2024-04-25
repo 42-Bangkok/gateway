9c42abd3989610a0745fa98ad702f140386ce980
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-04-25 13:51:24 +0000

    Add error handling to update_intraprofile task

--- app/apptasks/tasks/update_intra_profile.py
+++ app/apptasks/tasks/update_intra_profile.py
@@ -3,6 +3,7 @@ from celery import shared_task
 from apptasks.services.update_intraprofile import (
     update_intraprofile as _update_intraprofile,
 )
+from apptasks.tasks.discord import send_simple_message
 
 
 @shared_task
@@ -10,4 +11,10 @@ def update_intraprofile() -> bool:
     """
     Updates the intra profile of all cadets
     """
-    return _update_intraprofile()
+    try:
+        _update_intraprofile()
+    except Exception as e:
+        send_simple_message(f"update_intraprofile(); Error: {e}", "dev")
+        return False
+
+    return True
