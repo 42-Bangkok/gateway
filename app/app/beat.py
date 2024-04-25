57738582e2a38c880f697d70ec704f0ff16b477d
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-04-25 20:25:56 +0700

    Update beat.py schedule configuration

--- app/app/beat.py
+++ app/app/beat.py
@@ -14,20 +14,11 @@ syntax:
 from celery.schedules import crontab
 
 beat_schedule = {
+    # Configure using django-celery-beat is preferred
     # Add your tasks here example:
     # 'ping': {
     #     'task': 'appcore.tasks.sample.pong',
     #     'schedule': 10.0,
     #     'args': (),
     # }
-    "socialism": {
-        "task": "apptasks.tasks.communism.task_socialism",
-        "schedule": crontab(hour=7, minute=42, day_of_week=1),
-        "args": (),
-    },
-    "update_intra_profile": {
-        "task": "apptasks.tasks.intra.task_update_intra_profile",
-        "schedule": crontab(minute=0, hour=10),
-        "args": (),
-    },
 }
