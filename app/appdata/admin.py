93a40edec252ed3631f6d061d27f03ad9c83cd63
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-06-17 15:08:26 +0000

    chore: Register CadetMeta and IntraProfile models in admin.py

--- app/appdata/admin.py
+++ app/appdata/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from appdata.models.cadetmetas import CadetMeta
+from appdata.models.intras import IntraProfile
+
+
 # Register your models here.
+admin.site.register(CadetMeta)
+admin.site.register(IntraProfile)
